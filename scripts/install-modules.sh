199305942445c3859aa8ff8e41397fff136867b1
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2021-09-29 01:50:12 +0200

    fix react virtualized autosizer

--- scripts/install-modules.sh
+++ scripts/install-modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yarn
-
+cat ./modified-modules/react-virtualized/detectElementResize.js > ./node_modules/react-virtualized/dist/es/vendor/detectElementResize.js
 sed -i "s|container.cssRules.length|container?.cssRules?.length|g" node_modules/jss/dist/jss.esm.js
 sed -i "s|attach() {|attach() {return;|g" node_modules/jss/dist/jss.esm.js

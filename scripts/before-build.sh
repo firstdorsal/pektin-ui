97ef7307f3fcec5186c4273d5e5357ce7230c18f
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-02-09 22:31:05 +0100

    try to include short sha

--- scripts/before-build.sh
+++ scripts/before-build.sh
@@ -1,5 +1,7 @@
-if [[ -n $CI_COMMIT_SHORT_SHA ]]; then
-
-sed -i "s|CI_COMMIT_SHORT_SHA|${CI_COMMIT_SHORT_SHA }|g" ./public/index.html
+if [ ! -v ${1} ]; then
+echo $1
+sed -i "s|CI_COMMIT_SHORT_SHA|${1}|g" ./public/index.html
 
 fi
+
+# TODO add CI_COMMIT_SHORT_SHA replacer in CI

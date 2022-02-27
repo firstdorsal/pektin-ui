79da6f9732f97c061d5b3c1a31bb2008c5bf63f2
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@y.gy>
2022-02-27 01:51:16 +0100

    add ace code editor

--- src/components/ExecuteQuery.tsx
+++ src/components/ExecuteQuery.tsx
@@ -1,7 +1,15 @@
-import { Button, Container, Grid, Paper } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 import { Send } from "@material-ui/icons";
 import { Component } from "react";
 import { Config, Glob } from "./types";
+
+import AceEditor from "react-ace";
+//import "ace-builds/webpack-resolver";
+
+import "ace-builds/src-noconflict/theme-one_dark";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 interface ExecuteQueryProps {
   g: Glob;
@@ -10,16 +18,52 @@ interface ExecuteQueryProps {
 interface ExecuteQueryState {
   query: string;
 }
+
 export default class ExecuteQuery extends Component<ExecuteQueryProps, ExecuteQueryState> {
   state = {
     query: "",
   };
+
   render = () => {
     return (
       <div className="ExecuteQuery">
         <Container style={{ margin: "20px auto" }}>
           <Grid container spacing={3}>
             <Grid item xs={8}>
+              <AceEditor
+                mode="json"
+                theme="one_dark"
+                enableBasicAutocompletion={true}
+                enableLiveAutocompletion={true}
+                defaultValue={JSON.stringify(t, null, "    ")}
+                setOptions={{
+                  useWorker: false,
+
+                  highlightActiveLine: false,
+                  enableSnippets: true,
+                  highlightSelectedWord: false,
+                  showInvisibles: true,
+                  fadeFoldWidgets: true,
+                  displayIndentGuides: true,
+                  /*@ts-ignore*/
+                  scrollPastEnd: 1,
+                  fixedWidthGutter: true,
+                  dragEnabled: false,
+                  /*@ts-ignore*/
+                  newLineMode: "unix",
+                  useSoftTabs: true,
+                  wrap: true,
+                  showPrintMargin: false,
+                }}
+                onChange={(e) => {
+                  /* @ts-ignore */
+                  this.setState({ query: e?.target?.value });
+                }}
+                fontSize={16}
+                height="90vh"
+                width="70vw"
+                name="aceEditor"
+              />
               <Button variant="contained" color="primary" endIcon={<Send />}>
                 Send
               </Button>
@@ -31,3 +75,105 @@ export default class ExecuteQuery extends Component<ExecuteQueryProps, ExecuteQu
     );
   };
 }
+
+const t = {
+  services: {
+    ui: {
+      enabled: true,
+      domain: "pektin.club",
+      subDomain: "ui",
+    },
+    api: {
+      domain: "pektin.club",
+      subDomain: "api",
+    },
+    vault: {
+      domain: "pektin.club",
+      subDomain: "vault",
+    },
+    recursor: {
+      enabled: true,
+      domain: "pektin.club",
+      subDomain: "recursor",
+    },
+    ribston: {
+      enabled: true,
+    },
+    opa: {
+      enabled: false,
+    },
+  },
+  nodes: [
+    {
+      main: true,
+      name: "balduin",
+      ips: ["2a01:4f9:c01f:80::"],
+    },
+    {
+      name: "gustav",
+      ips: ["2a01:4f9:c01f:80::"],
+    },
+  ],
+  nameservers: [
+    { subDomain: "ns1", domain: "pektin.club", node: "balduin", main: true },
+    { subDomain: "ns2", domain: "pektin.club", node: "gustav" },
+    { subDomain: "ns1", domain: "toll.club", node: "balduin", main: true },
+  ],
+  letsencrypt: {
+    enabled: true,
+    letsencryptEmail: "test@pektin.club",
+  },
+  usePolicies: "ribston",
+  build: {
+    server: {
+      enabled: false,
+      path: "/home/paul/Documents/pektin/pektin-server",
+    },
+    api: {
+      enabled: false,
+      path: "/home/paul/Documents/pektin/pektin-api",
+    },
+    ui: {
+      enabled: false,
+      path: "/home/paul/Documents/pektin/pektin-ui",
+    },
+    ribston: {
+      enabled: false,
+      path: "/home/paul/Documents/pektin/ribston",
+    },
+    recursor: {
+      enabled: false,
+      path: "",
+    },
+    vault: {
+      enabled: false,
+      path: "",
+    },
+  },
+  reverseProxy: {
+    createTraefik: true,
+    routing: "local",
+    tls: false,
+    tempZone: { enabled: true, provider: "pektin.zone", routing: "local" },
+    traefikUi: { enabled: true, domain: "pektin.club", subDomain: "traefik" },
+    external: {
+      domain: "pektin.club",
+      subDomain: "pektin-proxy",
+      enabled: true,
+      services: [
+        {
+          name: "gandi",
+          enabled: true,
+          domain: "api.gandi.net",
+          accessControlAllowMethods: ["OPTIONS", "POST", "GET", "DELETE"],
+        },
+        {
+          name: "crt",
+          enabled: true,
+          domain: "crt.sh",
+          accessControlAllowMethods: ["OPTIONS", "GET"],
+        },
+      ],
+    },
+  },
+};
